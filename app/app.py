c0f04ca2d1c58f6fa4a55775c86236ebdb1ddd57
root <root@flask.dev.local>
2022-11-27 00:18:38 +0900

    supabaseの接続を追加

--- app/app.py
+++ app/app.py
@@ -1,13 +1,28 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template
+from supabase import create_client,Client
+import os
+
+url = os.environ[supabase]
+key = os.environ[supabase-apikey]
+
+supabase: Client = create_client(url, key)
+
+def find_all_data():
+    data = supabase.table("brewery").select("*").execute()
+    return data
+
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
 
+
+
 #「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/")
 def index():
-    return render_template("index.html")
+    hoge = find_all_data()
+    return render_template("index.html",data=hoge)
 
 
 #おまじない
