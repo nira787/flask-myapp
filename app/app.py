6196df4e463eedc5a9ae9e619a68e1008354d723
root <root@flask.dev.local>
2022-11-26 19:13:31 +0900

    add hello,world

--- app/app.py
+++ app/app.py
@@ -4,15 +4,8 @@ from flask import Flask,render_template
 #Flaskオブジェクトの生成
 app = Flask(__name__)
 
-
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
-@app.route("/")
-def hello():
-    return "Hello World"
-
-
 #「/index」へアクセスがあった場合に、「index.html」を返す
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template("index.html")
 
